2f6174daa4a871c0a266ee56d0d7f1008ea897c7
jasonscharff <jasonscharff@gmail.com>
2016-09-10 15:35:14 -0400

    Create project.

---
+++ PennAppsXIV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PennAppsXIV
+//
+//  Created by Jason Scharff on 9/10/16.
+//  Copyright © 2016 Jason Scharff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PennAppsXIV/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PennAppsXIV
+//
+//  Created by Jason Scharff on 9/10/16.
+//  Copyright © 2016 Jason Scharff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
