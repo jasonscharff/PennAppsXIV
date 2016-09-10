71aafa6fc4aacf40d15178d858d6b09b1f68f25f
jasonscharff <jasonscharff@gmail.com>
2016-09-10 16:01:23 -0400

    Begin the process of markdown rendering.

---
+++ PennAppsXIV/NTEMarkdownRenderController.h
@@ -0,0 +1,13 @@
+//
+//  NTEMarkdownRenderController.h
+//  PennAppsXIV
+//
+//  Created by Jason Scharff on 9/10/16.
+//  Copyright © 2016 Jason Scharff. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NTEMarkdownRenderController : NSObject
+
+@end
---
+++ PennAppsXIV/NTEMarkdownRenderController.m
@@ -0,0 +1,28 @@
+//
+//  NTEMarkdownRenderController.m
+//  PennAppsXIV
+//
+//  Created by Jason Scharff on 9/10/16.
+//  Copyright © 2016 Jason Scharff. All rights reserved.
+//
+
+#import "NTEMarkdownRenderController.h"
+
+#import <MMMarkdown/MMMarkdown.h>
+
+
+@implementation NTEMarkdownRenderController
+    
++ (NSString *)generateHTMLFromMarkdown : (NSString *)markdown {
+    //For now we'll do this.
+    //Next step is to do some regex on it first.
+    NSError *error;
+    NSString *html = [MMMarkdown HTMLStringWithMarkdown:markdown extensions:MMMarkdownExtensionsGitHubFlavored error:&error];
+    if(error) {
+        NSLog(@"error = %@", error);
+        //Show some alert.
+    }
+    return html;
+}
+
+@end
